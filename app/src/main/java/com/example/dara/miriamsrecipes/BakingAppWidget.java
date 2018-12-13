ac82d4a1ab571159992ca6c907af3536d8c7d706
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2018-12-13 15:02:04 +0100

    Create a widget

---
+++ app/src/main/java/com/example/dara/miriamsrecipes/BakingAppWidget.java
@@ -0,0 +1,52 @@
+package com.example.dara.miriamsrecipes;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.dara.miriamsrecipes.ui.list.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class BakingAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        //Create an intent to launch main activity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget);
+
+        //Handle clicks and launch main activity
+        views.setOnClickPendingIntent(R.id.appwidget_image, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
