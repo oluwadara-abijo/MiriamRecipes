a43f2e169b36e62c313e5741c370a0476ba89132
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2018-12-14 07:56:10 +0100

    Display ingredient list for desired recipe

--- app/src/main/java/com/example/dara/miriamsrecipes/BakingAppWidget.java
+++ app/src/main/java/com/example/dara/miriamsrecipes/BakingAppWidget.java
@@ -7,25 +7,51 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.dara.miriamsrecipes.data.model.Ingredient;
+import com.example.dara.miriamsrecipes.data.model.Recipe;
 import com.example.dara.miriamsrecipes.ui.list.MainActivity;
+
+import java.util.List;
+
+import static com.example.dara.miriamsrecipes.ui.list.MainActivity.EXTRA_RECIPE_ID;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class BakingAppWidget extends AppWidgetProvider {
 
+    private static Recipe mDesiredRecipe;
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         //Create an intent to launch main activity when clicked
         Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(EXTRA_RECIPE_ID, mDesiredRecipe);
+
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget);
 
         //Handle clicks and launch main activity
-        views.setOnClickPendingIntent(R.id.appwidget_image, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_tv_ingredients, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_image, pendingIntent);
+
+        //Get the ingredients of the desired recipe
+        List<Ingredient> mIngredients = mDesiredRecipe.getIngredients();
+
+        //Build a string containing all ingredients
+        StringBuilder allIngredients = new StringBuilder();
+
+        for (int i = 0; i < mIngredients.size(); i++) {
+            String ingredient = mIngredients.get(i).getIngredient();
+
+            allIngredients.append(ingredient).append("\n");
+        }
+
+        //Display the ingredients on the remote view
+        views.setTextViewText(R.id.widget_tv_ingredients, allIngredients);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -47,6 +73,10 @@ public class BakingAppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public static void setDesiredRecipe(Recipe desiredRecipe) {
+        BakingAppWidget.mDesiredRecipe = desiredRecipe;
     }
 }
 
--- app/src/main/java/com/example/dara/miriamsrecipes/ui/detail/RecipeStepActivity.java
+++ app/src/main/java/com/example/dara/miriamsrecipes/ui/detail/RecipeStepActivity.java
@@ -6,10 +6,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.example.dara.miriamsrecipes.BakingAppWidget;
 import com.example.dara.miriamsrecipes.R;
 import com.example.dara.miriamsrecipes.data.model.Recipe;
 import com.example.dara.miriamsrecipes.data.model.Step;
@@ -109,5 +114,22 @@ public class RecipeStepActivity extends AppCompatActivity implements MasterListA
             startActivity(intent);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.recipe_detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_add_to_widget) {
+            BakingAppWidget.setDesiredRecipe(mRecipe);
+            Toast.makeText(this, "Recipe added to widget", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
